971fb7ee92b27c78a7c3637d51a8413712de87e1
kasarlaKS <kasarlaks@kasarlaKSs-MBP.lan>
2023-12-10 23:31:20 -0600

    changes done successfully

--- Databasecode/Food Truck.sql
+++ Databasecode/Food Truck.sql
@@ -1,89 +1,102 @@
+drop database food_truck;
 create database food_truck;
 use food_truck;
 
-create table food_truck(
-food_truck_id int auto_increment primary key,
-name varchar(255) not null,
-email varchar(255) not null unique,
-password varchar(255) not null,
-phone varchar(255) not null,
-verification_status varchar(255) default 'not verified'
-);
-alter table food_truck add food_truck_title varchar(255) not null;
-create table delivery_boy(
-delivery_boy_id int auto_increment primary key,
-name varchar(255) not null,
-email varchar(255) not null unique,
-password varchar(255) not null,
-phone varchar(255) not null,
-verification_status varchar(255) default 'not verified'
-);
-alter table delivery_boy add address_proof varchar(255) not null;
-create table customers(
-customer_id int auto_increment primary key,
-name varchar(255) not null,
-email varchar(255) not null unique,
-password varchar(255) not null,
-phone varchar(255) not null,
-gender varchar(255) not null,
-address varchar(255) not null,
-verification_status varchar(255) default 'not verified'
-);
-create table truck_timings(
-truck_timing_id int auto_increment primary key,
-from_time varchar(255) not null,
-to_time varchar(255) not null,
-date varchar(255) not null,
-location varchar(255) not null,
-food_truck_id int not null,
-foreign key(food_truck_id) references food_truck(food_truck_id)
-);
-create table categories(
-category_id int auto_increment primary key,
-category_name varchar(255) not null,
-food_truck_id int not null,
-foreign key(food_truck_id) references food_truck(food_truck_id)
-);
-create table food_items(
-food_item_id int auto_increment primary key,
-food_item_name varchar(255) not null,
-price          varchar(255) not null,
-quantity       int not null,
-units          varchar(255) not null,
-category_id    int not null,
-picture        varchar(255) not null,
-status         varchar(255) not null,
-description    varchar(255) not null,
-foreign key(category_id) references categories(category_id)
-);
-alter table food_items modify status varchar(255) default 'Available';
-create table customer_orders(
-customer_order_id int auto_increment primary key,
-status varchar(255) not null,
-customer_id int not null,
-date datetime default current_timestamp,
-delivery_boy_id int ,
-food_truck_id int not null,
-truck_timing_id int not null,
-foreign key(delivery_boy_id) references delivery_boy(delivery_boy_id),
-foreign key(customer_id) references customers(customer_id),
-foreign key(food_truck_id) references food_truck(food_truck_id),
-foreign key(truck_timing_id) references truck_timings(truck_timing_id)
+CREATE TABLE food_truck (
+    food_truck_id INT AUTO_INCREMENT PRIMARY KEY,
+    food_truck_title VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    verification_status VARCHAR(255) DEFAULT 'not verified'
 );
 
-create table customer_order_items(
-customer_order_item_id int auto_increment primary key,
-customer_order_id int not null,
-food_item_id int not null,
-quantity int not null,
-foreign key(customer_order_id) references customer_orders(customer_order_id),
-foreign key(food_item_id) references food_items(food_item_id)
+CREATE TABLE delivery_boy (
+    delivery_boy_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    address_proof VARCHAR(255) NOT NULL,
+    verification_status VARCHAR(255) DEFAULT 'not verified'
 );
 
-create table review(
-review_id int auto_increment primary key,
-customer_order_item_id int not null,
-review varchar(255) not null,
-rating varchar(255) not null,
-foreign key(customer_order_item_id) references customer_order_items(customer_order_item_id)
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    verification_status VARCHAR(255) DEFAULT 'not verified'
+);
+CREATE TABLE truck_timings (
+    truck_timing_id INT AUTO_INCREMENT PRIMARY KEY,
+    from_time VARCHAR(255) NOT NULL,
+    to_time VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    food_truck_id INT NOT NULL,
+    FOREIGN KEY (food_truck_id)
+        REFERENCES food_truck (food_truck_id)
+);
+CREATE TABLE categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    food_truck_id INT NOT NULL,
+    FOREIGN KEY (food_truck_id)
+        REFERENCES food_truck (food_truck_id)
+);
+CREATE TABLE food_items (
+    food_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    food_item_name VARCHAR(255) NOT NULL,
+    price VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    units VARCHAR(255) NOT NULL,
+    category_id INT NOT NULL,
+    picture VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL default 'Available',
+    description VARCHAR(255) NOT NULL,
+    FOREIGN KEY (category_id)
+        REFERENCES categories (category_id)
+);
+alter table food_items modify status VARCHAR(255) NOT NULL default 'Available';
+CREATE TABLE customer_orders (
+    customer_order_id INT AUTO_INCREMENT PRIMARY KEY,
+    status VARCHAR(255) NOT NULL,
+    customer_id INT NOT NULL,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    delivery_boy_id INT,
+    food_truck_id INT NOT NULL,
+    truck_timing_id INT NOT NULL,
+    FOREIGN KEY (delivery_boy_id)
+        REFERENCES delivery_boy (delivery_boy_id),
+    FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id),
+    FOREIGN KEY (food_truck_id)
+        REFERENCES food_truck (food_truck_id),
+    FOREIGN KEY (truck_timing_id)
+        REFERENCES truck_timings (truck_timing_id)
+);
+
+CREATE TABLE customer_order_items (
+    customer_order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_order_id INT NOT NULL,
+    food_item_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (customer_order_id)
+        REFERENCES customer_orders (customer_order_id),
+    FOREIGN KEY (food_item_id)
+        REFERENCES food_items (food_item_id)
+);
+
+CREATE TABLE review (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_order_item_id INT NOT NULL,
+    review VARCHAR(255) NOT NULL,
+    rating VARCHAR(255) NOT NULL,
+    FOREIGN KEY (customer_order_item_id)
+        REFERENCES customer_order_items (customer_order_item_id)
 );
